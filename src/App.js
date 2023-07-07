b858f40c5afbb2632d4449aba3c8ef225977a8f8
George Heeres <gheeres@gmail.com>
2023-07-06 23:35:01 -0500

    Week 14 - Assignment: Stars Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React from 'react';
 import './App.css';
 import ProfileList from './components/ProfileList';
+import Stars from './components/Stars';
 
-function App() {
-  return (
-    <section className="profile vh-100">
-      <div className="container py-5 h-100">
-        <ProfileList />
-      </div>
-    </section>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 0
+    };
+  }
+
+  #handleRatingChange = (e) => {
+    console.log(`Rating changed...${ e }`)
+    if (e) {
+      this.setState({ rating: e })
+      //this.state.rating = e;
+      //this.render();
+    }
+  }
+
+  render() {
+    const rating = this.state.rating;
+
+    return (
+      <section className="profile vh-100">
+        <div className="container py-5 h-100">
+          <Stars rating={ rating } color={ '#f00' } onRating={ this.#handleRatingChange } />
+          <ProfileList />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Stars.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const defaultMaxRating = 5;
+
+export default class Stars extends React.Component {
+  constructor(props) {
+    super(props);
+  } 
+
+  #handleClick = (e) => {
+    if (e.target) {
+      let rating = e.target.dataset.rating;
+      if (rating) {
+        if (this.props.onRating) {
+          this.props.onRating(rating);  
+        }
+      }
+    }
+  }
+
+  getColorForRating(rating) {
+    switch(rating) {
+      case 1:
+      case 2:  
+        return '#f00';
+      case 3:  
+        return '#FFA500';
+      case 4:  
+        return '#cc9900';
+      case 5:  
+        return '#008000';
+    }
+    return '#000';
+  }
+
+  render() {
+    let rating = parseInt(this.props.rating || 0, 10);
+    let maxRating = this.props.maxRating || defaultMaxRating;
+    let color = this.getColorForRating(rating);
+    console.log(`color=${ color }`);
+
+    let items = [];
+    for(let i = 1; i <= maxRating; i++) {
+      items.push(
+        <i key={ i } data-rating={ i } style={ { color: color } } className={ `bi bi-star${ (rating >= i) ? '-fill' : '' }`}></i>
+      ) ; 
+    }
+
+    return (
+      <div onClick={ this.#handleClick } >
+        { items }
+      </div>
+    );
+  }
+}
